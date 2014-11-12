4088fedbc4d2bff15b6ff36eda2793beff61d3ac
Christophe Van Neste <Christophe.VanNeste@UGent.be>
2014-11-12 08:54:41 +0100

    rmne removed for rmne app see https://github.ugent.be/fvnieuwe/rmne

--- src/MyFLsite/rmne/bin/rmne.py
+++
@@ -1,1 +0,0 @@
-testrmne = 'afhuehlrafiaehfuihafuihauifhuiheafi'
--- src/MyFLsite/rmne/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required#, permission_required
-from django.views.generic import TemplateView
-
-urlpatterns = patterns('',
-    #All keys in kwargs need to be strings, with TemplateView they are passed to the context
-    url(r'^$', 'rmne.views.calcform'), 
-    url(r'^help/$', TemplateView.as_view(template_name='rmne/help.html'),kwargs={'rmne':True}), 
-)
